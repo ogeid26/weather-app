886c4d00d2fe59f2235729bde3b215328bb9b27e
ogeid26 <dmalvaradog26@gmail.com>
2023-06-06 00:22:31 -0300

    footer changes

--- src/components/weather-footer.svelte
+++ src/components/weather-footer.svelte
@@ -1,38 +1,44 @@
 <footer>
-    <div>
-        <span>
-            33%
-        </span>
-        <span>
-            Humidity
-        </span>
-    </div>
+  <div>
+    <span> 33% </span>
+    <strong> Humidity </strong>
+  </div>
 
-    <div>
-        <span>
-            33%
-        </span>
-        <span>
-            Wind speed
-        </span>
-    </div>
+  <div>
+    <span> 33% </span>
+    <strong> Wind speed </strong>
+  </div>
 
-    <div>
-        <span>
-            12°
-        </span>
-        <span>
-            Feels like
-        </span>
-    </div>
-
+  <div>
+    <span> 12° </span>
+    <strong> Feels like </strong>
+  </div>
 </footer>
 
 <style>
-    footer {
-        border: 2px solid #000;
-        padding: 32px;
-        width: 50;
-    }
+  footer {
+    border: 2px solid #000;
+    padding: 12px 32px;
+    width: 80%;
+    margin: 0 auto;
+    border-radius: 10px;
+    display: flex;
+    text-align: center;
+    justify-content: space-evenly;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    bottom: 32px;
+  }
 
+  span {
+    display: block;
+  }
+
+  strong {
+    color: #444;
+    font-size: 14px;
+    font-weight: 600;
+  }
 </style>
